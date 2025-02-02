90863031e25c96688f9c0ed7685e953996b6b632
Jagoda Kuczkowska <jagoda.kucz@wp.pl>
2025-02-02 21:07:01 +0100

    updated user dto

--- src/module/users/dto/register.dto.ts
+++ src/module/users/dto/register.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, IsEmail, IsNotEmpty } from 'class-validator';
+import { IsString, IsEmail, IsNotEmpty, MinLength, Matches, MaxLength } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 
@@ -6,25 +6,32 @@ export class RegisterDto {
   @ApiProperty()
   @IsString()
   @IsNotEmpty()
+  @MaxLength(30)
   username: string;
 
   @ApiProperty()
   @IsString()
   @IsNotEmpty()
+  @MinLength(8)
+  @MaxLength(25)
+  @Matches(/^(?=.*[!@#$%^&*(),.?":{}|<>]).*$/, { message: 'password must contain at least one special character' })
   password: string;
 
   @ApiProperty()
   @IsEmail()
   @IsNotEmpty()
+  @MaxLength(50)
   email: string;
 
   @ApiProperty()
   @IsString()
   @IsNotEmpty()
+  @MaxLength(30)
   firstName: string;
 
   @ApiProperty()
   @IsString()
   @IsNotEmpty()
+  @MaxLength(50)
   lastName: string;
 }
--- src/module/users/dto/sign-in.dto.ts
+++ src/module/users/dto/sign-in.dto.ts
@@ -1,14 +1,18 @@
-import { IsString, IsEmail, IsNotEmpty } from 'class-validator';
+import { IsString, IsEmail, IsNotEmpty, MinLength, MaxLength, Matches } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class SignInDto {
     @ApiProperty()
     @IsString()
     @IsNotEmpty()
+    @MaxLength(30)
     username: string;
 
     @ApiProperty()
     @IsString()
     @IsNotEmpty()
+    @MinLength(8)
+    @MaxLength(25)
+    @Matches(/^(?=.*[!@#$%^&*(),.?":{}|<>]).*$/, { message: 'password must contain at least one special character' })
     password: string;
 }
